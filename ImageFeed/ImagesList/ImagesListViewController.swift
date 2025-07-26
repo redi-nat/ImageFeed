82a366714be0326ff2d3776309509de9ac355b8a
Natalia <nredinskaya@gmail.com>
2025-07-26 18:16:33 +0200

    Sprint 9 done

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -1,29 +1,41 @@
-//
-//  ViewController.swift
-//  ImageFeed
-//
-//  Created by Natalia Redinskaia on 09/07/2025.
-//
-
 import UIKit
 
 final class ImagesListViewController: UIViewController {
-    
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
+
     @IBOutlet private var tableView: UITableView!
-    
+
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
-    
+
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
         formatter.timeStyle = .none
         return formatter
     }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        tableView.rowHeight = 200
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+        
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showSingleImageSegueIdentifier {
+            guard
+                let viewController = segue.destination as? SingleImageViewController,
+                let indexPath = sender as? IndexPath
+            else {
+                assertionFailure("Invalid segue destination")
+                return
+            }
+            
+            let image = UIImage(named: photosName[indexPath.row])
+            viewController.image = image
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
     }
 }
 
@@ -65,16 +77,18 @@ extension ImagesListViewController {
 }
 
 extension ImagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        guard let image = UIImage(named: photosName[indexPath.row]) else {
-            return 0
-        }
-        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
-        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
-        let imageWidth = image.size.width
-        let scale = imageViewWidth / imageWidth
-        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
-        return cellHeight
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ShowSingleImage", sender: indexPath)
     }
-}
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            guard let image = UIImage(named: photosName[indexPath.row]) else {
+                return 0
+            }
+            let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+            let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+            let imageWidth = image.size.width
+            let scale = imageViewWidth / imageWidth
+            let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+            return cellHeight
+        }
+    }
--- ImageFeed/ProfileViewController.swift
+++ ImageFeed/ProfileViewController.swift
@@ -1,5 +1,103 @@
-
 import UIKit
 
 final class ProfileViewController: UIViewController {
+   
+    private var imageView: UIImageView!
+    private var nameLabel: UILabel!
+    private var loginLabel: UILabel!
+    private var descriptionLabel: UILabel!
+    private var exitButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(named: "YP Black")
+        setupImageView()
+        setupExitButton()
+        setupNameLabel()
+        setupLoginLabel()
+        setupDescriptionLabel()
+    }
+
+    private func setupImageView() {
+        imageView = UIImageView(image: UIImage(named: "avatar"))
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 35
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            imageView.widthAnchor.constraint(equalToConstant: 70),
+            imageView.heightAnchor.constraint(equalToConstant: 70)
+        ])
+    }
+    
+    private func setupExitButton() {
+        exitButton = UIButton.systemButton(
+            with: UIImage(systemName: "ipad.and.arrow.forward")!,
+            target: self,
+            action: #selector(didTapButton)
+        )
+        exitButton.tintColor = UIColor(named: "YP Red")
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(exitButton)
+
+        NSLayoutConstraint.activate([
+            exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            exitButton.centerYAnchor.constraint(equalTo: imageView.centerYAnchor)
+        ])
+    }
+
+    private func setupNameLabel() {
+        nameLabel = UILabel()
+        nameLabel.text = "Ekaterina Novikova"
+        nameLabel.textColor = .white
+        nameLabel.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        
+        NSLayoutConstraint.activate([
+            nameLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
+            nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20)
+        ])
+    }
+
+    private func setupLoginLabel() {
+        loginLabel = UILabel()
+        loginLabel.text = "@e_novikova"
+        loginLabel.textColor = UIColor(named: "YP Gray")
+        loginLabel.font = UIFont.systemFont(ofSize: 13)
+        loginLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(loginLabel)
+
+        NSLayoutConstraint.activate([
+            loginLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
+            loginLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8)
+        ])
+    }
+
+    private func setupDescriptionLabel() {
+        descriptionLabel = UILabel()
+        descriptionLabel.text = "Hello, world!"
+        descriptionLabel.textColor = .white
+        descriptionLabel.font = UIFont.systemFont(ofSize: 13)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabel)
+
+        NSLayoutConstraint.activate([
+            descriptionLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
+            descriptionLabel.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 8)
+        ])
+    }
+
+    @objc
+    private func didTapButton() {
+        imageView.image = UIImage(named: "UserImage")
+   
+        nameLabel.removeFromSuperview()
+        loginLabel.removeFromSuperview()
+        descriptionLabel.removeFromSuperview()
+    }
 }
